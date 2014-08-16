adcf849d4075e64a34efca47d682dccbe502fe68
Élie Bouttier <elie@bouttier.eu>
2014-08-15 17:59:53 -0700

    runserver: run with default settings

--- runserver.sh
+++ runserver.sh
@@ -2,4 +2,8 @@
 
 . env/bin/activate
 
-python manage.py runserver 0.0.0.0:8000 --settings ponytracker.local_settings
+if [ "$1" == 'prod' ]; then
+  python manage.py runserver 0.0.0.0:8000 --settings ponytracker.local_settings
+else
+  python manage.py runserver 0.0.0.0:8000
+fi
