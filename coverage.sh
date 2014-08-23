313219b6ebdfe1f7248c69abfab4f541878e98c0
Élie Bouttier <elie@bouttier.eu>
2014-08-22 17:03:41 -0700

    coverage.sh: install coverage if not installed

--- coverage.sh
+++ coverage.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# you must install coverage before executing this script
-#   pip install coverage
-
 . env/bin/activate
+
+which coverage >/dev/null 2>&1
+if [ "$?" -ne 0 ]; then
+  pip install coverage
+fi
 
 coverage run manage.py test
 coverage report
