c0e1dcdde06ca52ed20eac0d9d2246ebea39ae53
Élie Bouttier <elie@bouttier.eu>
2014-08-22 18:49:23 -0700

    improve runserver.sh

--- runserver.sh
+++ runserver.sh
@@ -2,8 +2,27 @@
 
 . env/bin/activate
 
-if [ "$1" == 'prod' ]; then
-  python manage.py runserver 0.0.0.0:8000 --settings ponytracker.local_settings
+cmd="python manage.py runserver"
+
+function usage() {
+  echo "Usage: $0 [default|local]"
+  exit 1
+}
+
+if [[ "$#" -gt 2 ]]; then
+  usage
+fi
+
+conf="$1"
+
+if [ "$conf" == "" ]; then
+  conf="default"
+fi
+
+if [ "$conf" == 'default' ]; then
+  $cmd
+elif [ "$conf" == 'local' ]; then
+  $cmd --settings ponytracker.local_settings
 else
-  python manage.py runserver 0.0.0.0:8000
+  usage
 fi
