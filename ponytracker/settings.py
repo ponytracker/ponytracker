a889c3f0adbd900898b346387b87a315081b750c
Élie Bouttier <elie@bouttier.eu>
2015-02-17 22:50:03 +0100

    link to documentation at readthedocs

--- ponytracker/settings.py
+++ ponytracker/settings.py
@@ -6,6 +6,9 @@ https://docs.djangoproject.com/en/dev/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/dev/ref/settings/
+
+For ponytracker specific settings and their values, see
+https://ponytracker.readthedocs.org/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
