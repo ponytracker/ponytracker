d14861a33b947ff29c03922e46870199daf554cf
Élie Bouttier <elie@bouttier.eu>
2016-01-09 15:20:03 +0100

    missing migration

---
+++ tracker/migrations/0008_auto_20160109_1419.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tracker', '0007_milestone_deleted'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='label',
+            name='project',
+            field=models.ForeignKey(to='tracker.Project', related_name='+'),
+        ),
+        migrations.AlterField(
+            model_name='milestone',
+            name='project',
+            field=models.ForeignKey(to='tracker.Project', related_name='+'),
+        ),
+    ]
